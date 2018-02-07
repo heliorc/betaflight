cff300ed1a5b0adc580464dde022df23189bcfb5
Tim Sweet <timothyswt@gmail.com>
2018-02-07 14:40:29 -0700

    making sausage

--- src/main/target/HELIOSPRING/target.c
+++ src/main/target/HELIOSPRING/target.c
@@ -26,34 +26,16 @@
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
    
-    // DEF_TIM(TIM4, CH1,  PB6, TIM_USE_ANY,                 0, 0), //   CAMERA_CONTROL_PIN
+    DEF_TIM(TIM4, CH1,  PB6, TIM_USE_ANY,                 0, 0), //   CAMERA_CONTROL_PIN
 
     // Motors
-    DEF_TIM(TIM8,  CH1, PC6,  TIM_USE_MOTOR,               0, 0), // S1_OUT D1_ST7
-    DEF_TIM(TIM8,  CH2, PC7,  TIM_USE_MOTOR,               0, 0), // S2_OUT D1_ST2
-    DEF_TIM(TIM8,  CH3, PC8,  TIM_USE_MOTOR,               0, 0), // S3_OUT D1_ST6
-    DEF_TIM(TIM8,  CH4, PC9,  TIM_USE_MOTOR,               0, 0), // S4_OUT D1_ST1
+    DEF_TIM(TIM3,  CH1, PC6,  TIM_USE_MOTOR,               0, 0), // S1_OUT D1_ST7
+    DEF_TIM(TIM3,  CH2, PC7,  TIM_USE_MOTOR,               0, 0), // S2_OUT D1_ST2
+    DEF_TIM(TIM3,  CH3, PC8,  TIM_USE_MOTOR,               0, 0), // S3_OUT D1_ST6
+    DEF_TIM(TIM3,  CH4, PC9,  TIM_USE_MOTOR,               0, 0), // S4_OUT D1_ST1
 
     // LED strip
     DEF_TIM(TIM1,  CH1, PA8,  TIM_USE_LED, 0, 0), // D1_ST0
     // PPM?
     DEF_TIM(TIM12, CH1, PB14, TIM_USE_PPM,   0, 0),
-    // DEF_TIM(TIM1, CH2, PA9,  TIM_USE_PWM | TIM_USE_PPM,   0, 0), // PPM
-
-    // // // UART Backdoors, needed??
-
-    // DEF_TIM(TIM1,  CH3, PA9,  TIM_USE_NONE,                0, 0), // TX1
-    // DEF_TIM(TIM1,  CH4, PA10,  TIM_USE_NONE,                0, 0), // RX1
-
-    // DEF_TIM(TIM2,  CH3, PA1,  TIM_USE_NONE,                0, 0), // TX2
-    // DEF_TIM(TIM2,  CH4, PA2,  TIM_USE_NONE,                0, 0), // RX2
-
-    // DEF_TIM(TIM3,  CH3, PB10, TIM_USE_NONE,                0, 0), // TX3
-    // DEF_TIM(TIM3,  CH4, PB11, TIM_USE_NONE,                0, 0), // RX3
-
-    // DEF_TIM(TIM4,  CH3, PC10, TIM_USE_NONE,                0, 0), // TX4
-    // DEF_TIM(TIM4,  CH4, PC11, TIM_USE_NONE,                0, 0), // RX4
-
-    // DEF_TIM(TIM5,  CH3, PD2, TIM_USE_NONE,                0, 0), // TX5
-    // DEF_TIM(TIM5,  CH4, PD3, TIM_USE_NONE,                0, 0), // RX5
 };
--- src/main/target/HELIOSPRING/target.h
+++ src/main/target/HELIOSPRING/target.h
@@ -49,6 +49,7 @@
 
 
 // MPU6000 interrupts
+#define USE_FAKE_GYRO
 #define USE_IMUF
 #define USE_EXTI
 #define MPU_INT_EXTI            PC4
@@ -144,5 +145,5 @@
 #define TARGET_IO_PORTC         0xffff
 #define TARGET_IO_PORTD         (BIT(2))
 
-#define USABLE_TIMER_CHANNEL_COUNT 11
-#define USED_TIMERS             ( TIM_N(1) | TIM_N(4) | TIM_N(8) | TIM_N(12) )
+#define USABLE_TIMER_CHANNEL_COUNT 7
+#define USED_TIMERS             ( TIM_N(1) | TIM_N(3) | TIM_N(4) | TIM_N(12) )
